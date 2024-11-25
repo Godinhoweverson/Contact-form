7a9e955a2c70238a4c8b4702c89dd87b9767143c
Weverson Godinho <weverson.godinho@icloud.com>
2024-11-25 15:47:17 +0000

    Create a function errorMessage that will be call when the input is empty creating a p element to display a message and add a border

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,84 @@
 const firstName = document.getElementById('first_name');
 const lastName = document.getElementById('last_name');
 const email = document.getElementById('email');
-const queriesRadio = document.getElementsByName('query');
-const checkbox = document.getElementsByName('checkbox');
+const queriesRadio = document.querySelectorAll('#query');
+const containerQueryTypes = document.querySelector('.container-query-types');
+const checkbox = document.getElementById('checkbox');
 const message = document.getElementById('message');
 const btnSubmit = document.getElementById('btn-submit');
 
 
 // Test if the email is valid
-
-
-
 btnSubmit.addEventListener('click', () =>{
-    firstName.style = '';
-    const required = document.querySelectorAll('.required');
+    //Reset all elements
+
+    checkbox.parentNode.style = '';
+    const required = [...document.querySelectorAll('.required')];
     if(required){
-        console.log(required)
-        required.remove();
+        required.forEach((e) =>{
+            e.remove();
+        });
+    };
+        
+    const errorMessage = (el, message) =>{
+        const p = document.createElement('p');
+        p.setAttribute('class', 'required');
+        p.style = 'margin-top:8px'
+        p.style.color = '#D73C3C';
+        p.innerHTML = message;
+        el.insertAdjacentElement('afterend', p);
+        el.style = 'border:1px solid #D73C3C !important';
     }
-// Validate First name
-if(firstName.value === ''){
-    const firstnameRequired = document.createElement('p');
-    firstnameRequired.setAttribute('class', 'required');
-    firstnameRequired.style.color = '#D73C3C';
-    firstnameRequired.innerHTML = 'This field is required'
-    firstName.insertAdjacentElement('afterend', firstnameRequired);
-    firstName.style = 'border:1px solid #D73C3C !important';
-}
-//  validate Email
 
-const e = email.value;
+    // Validate First name
+    if(firstName.value === ''){
+        errorMessage(firstName, 'This field is required')
+    }
+
+    // Validate last Name
+    if(lastName.value === ''){
+        errorMessage(lastName, 'This field is required')
+    }
+
+    //  validate Email
+    const e = email.value;
 
 
-function validateEmail(e){
-    const patt = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return patt.test(e);
-};
+    function validateEmail(e){
+        const patt = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return patt.test(e);
+    };
 
-    if(validateEmail(e)){
-        console.log('valid email address');
-    }else{
-        const emailError = document.createElement('p');
-        emailError.setAttribute('class', 'required');
-        emailError.style.color = '#D73C3C';
-        emailError.innerHTML = 'Please enter a valid email address'
-        email.insertAdjacentElement('afterend', emailError);
-        email.style = 'border:1px solid #D73C3C !important';
+        if(validateEmail(e)){
+            console.log('valid email address');
+        }else{
+            errorMessage(email, 'Please enter a valid email address')
+        }
+
+    //Queries 
+    if(!queriesRadio[0].checked && !queriesRadio[1].checked){
+        const p = document.createElement('p');
+        p.setAttribute('class', 'required');
+        p.style = 'margin-top:8px'
+        p.style.color = '#D73C3C';
+        p.innerHTML = 'Please select a query type';
+        containerQueryTypes.insertAdjacentElement('afterend', p);
+    };
+
+    // Validate Message
+    if(message.value === ''){
+        errorMessage(message, 'This field is required')
+    };
+
+    if(!checkbox.checked){
+        const p = document.createElement('p');
+        p.setAttribute('class', 'required');
+        p.style = 'margin:8px 0 35px 0'
+        p.style.color = '#D73C3C';
+        p.innerHTML = 'To submit this form, please consent to being contacted';
+        checkbox.parentNode = 'margin: 0px !important'
+        checkbox.parentNode.insertAdjacentElement('afterend', p);
+        checkbox.parentNode.style = 'margin-bottom:0px';
     }
 });
 
-// error states messages
