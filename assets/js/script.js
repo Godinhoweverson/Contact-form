bde55e846b9d2405afb3ecae7dc0909965668736
Weverson Godinho <weverson.godinho@icloud.com>
2024-11-27 15:48:10 +0000

    Complete the application

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,9 @@ const message = document.getElementById('message');
 const btnSubmit = document.getElementById('btn-submit');
 const caixaMessage = document.getElementById('caixa-message');
 
-// Test if the email is valid
 
-const validForm = () =>{
-    //Reset all elements
-
+//Reset All Elements
+const resetEl = () => {
     checkbox.parentNode.style = '';
     const required = [...document.querySelectorAll('.required')];
     if(required){
@@ -21,83 +19,116 @@ const validForm = () =>{
             e.remove();
         });
     };
-        
-    const errorMessage = (el, message) =>{
-        const p = document.createElement('p');
-        p.setAttribute('class', 'required');
-        p.style = 'margin-top:8px'
-        p.style.color = '#D73C3C';
-        p.innerHTML = message;
-        el.insertAdjacentElement('afterend', p);
-        el.style = 'border:1px solid #D73C3C !important';
-    }
-
-    // Validate First name
-    if(firstName.value === ''){
-        errorMessage(firstName, 'This field is required')
-    }else{
-        firstName.style = '';
-    }
-
-    // Validate last Name
-    if(lastName.value === ''){
-        errorMessage(lastName, 'This field is required')
-    }else{
-        lastName.style = '';
-    }
-
-    //  validate Email
-    const e = email.value;
+    firstName.style = '';
+    lastName.style = '';
+    email.style = '';
+    message.style = '';
+    firstName.value = '';
+    lastName.value = '';
+    email.value = '';
+    message.value = '';
+    checkbox.checked = false;
+    queriesRadio[0].checked = false;
+    queriesRadio[1].checked = false;
+};
+ 
+const errorMessage = (el, message) =>{
+    const p = document.createElement('p');
+    p.setAttribute('class', 'required');
+    p.style = 'margin-top:8px'
+    p.style.color = '#D73C3C';
+    p.innerHTML = message;
+    el.insertAdjacentElement('afterend', p);
+    el.style = 'border:1px solid #D73C3C !important';
+}
 
 
-    function validateEmail(e){
-        const patt = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return patt.test(e);
-    };
+  // Test if the email is valid
 
-        if(validateEmail(e)){
+function validateEmail(e){
+    const patt = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return patt.test(e);
+};
+const getValueEmail = () =>{
+    let e = email.value;
+    return e
+};
+
+const validForm = () =>{ 
+
+    let validEmail = validateEmail(getValueEmail());
+  
+    if(validEmail !== true || firstName.value === '' || lastName.value === '' || message.value === '' || !checkbox.checked ||(!queriesRadio[0].checked && !queriesRadio[1].checked)){
+        checkbox.parentNode.style = '';
+        const required = [...document.querySelectorAll('.required')];
+        if(required){
+            required.forEach((e) =>{
+                e.remove();
+            });
+        };
+        // Validate First name
+        if(firstName.value === ''){
+            errorMessage(firstName, 'This field is required')
+        }else{
+            firstName.style = '';
+        };
+
+        // Validate last Name
+        if(lastName.value === ''){
+            errorMessage(lastName, 'This field is required')
+        }else{
+            lastName.style = '';
+        };
+
+        if(validEmail){
             email.style = ''
         }else{
             errorMessage(email, 'Please enter a valid email address')
-        }
+        };
 
-    //Queries 
-    if(!queriesRadio[0].checked && !queriesRadio[1].checked){
-        const p = document.createElement('p');
-        p.setAttribute('class', 'required');
-        p.style = 'margin-top:8px'
-        p.style.color = '#D73C3C';
-        p.innerHTML = 'Please select a query type';
-        containerQueryTypes.insertAdjacentElement('afterend', p);
-    }
+        //Queries 
+        if(!queriesRadio[0].checked && !queriesRadio[1].checked){
+            const p = document.createElement('p');
+            p.setAttribute('class', 'required');
+            p.style = 'margin-top:8px'
+            p.style.color = '#D73C3C';
+            p.innerHTML = 'Please select a query type';
+            containerQueryTypes.insertAdjacentElement('afterend', p);
+        };
 
-    // Validate Message
-    if(message.value === ''){
-        errorMessage(message, 'This field is required')
+        // Validate Message
+        if(message.value === ''){
+            errorMessage(message, 'This field is required')
+        }else{
+            message.style ='';
+        };
+
+        if(!checkbox.checked){
+            const p = document.createElement('p');
+            p.setAttribute('class', 'required');
+            p.style = 'margin:8px 0 35px 0'
+            p.style.color = '#D73C3C';
+            p.innerHTML = 'To submit this form, please consent to being contacted';
+            checkbox.parentNode = 'margin: 0px !important'
+            checkbox.parentNode.insertAdjacentElement('afterend', p);
+            checkbox.parentNode.style = 'margin-bottom:0px';
+        };
+        return false;
     }else{
-        message.style ='';
-    }
+        resetEl();
+        return true;
+    };  
+};
 
-    if(!checkbox.checked){
-        const p = document.createElement('p');
-        p.setAttribute('class', 'required');
-        p.style = 'margin:8px 0 35px 0'
-        p.style.color = '#D73C3C';
-        p.innerHTML = 'To submit this form, please consent to being contacted';
-        checkbox.parentNode = 'margin: 0px !important'
-        checkbox.parentNode.insertAdjacentElement('afterend', p);
-        checkbox.parentNode.style = 'margin-bottom:0px';
-    }
-}
 
-btnSubmit.addEventListener('submit', () =>{
-   validForm();
-   caixaMessages();
+btnSubmit.addEventListener('click', (event) =>{
+    event.preventDefault();
+    if(validForm()){
+        caixaMessage.style.display = 'flex';
+        setTimeout(() =>{
+            caixaMessage.style.display = 'none';
+        },2000);    
+    };
 });
-
-const caixaMessages = () =>{
-    console.log(caixaMessage)
-    caixaMessage.style.display = 'flex';
-}
 
 
